136f5575b2ecf76ffde2c38639d1cd3a5f2b0d45
Alberto Pou <albertopouquiros@gmail.com>
2021-03-24 16:03:29 +0100

    Update urls

--- billing_datatrans/urls.py
+++ billing_datatrans/urls.py
@@ -1,10 +1,10 @@
-from django.conf.urls import url
+from django.urls import path
 
 from .views import InitiateRegisterCreditCardView, InitiateReloadAccountView
 
 urlpatterns = [
-    url(r'^initiate-reload-account$', InitiateReloadAccountView.as_view(),
-        name='billing_datatrans_initiate_reload_account'),
-    url(r'^initiate-register-credit-card$', InitiateRegisterCreditCardView.as_view(),
-        name='billing_datatrans_initiate_register_credit_card'),
+    path(r'initiate-reload-account', InitiateReloadAccountView.as_view(),
+         name='billing_datatrans_initiate_reload_account'),
+    path(r'initiate-register-credit-card', InitiateRegisterCreditCardView.as_view(),
+         name='billing_datatrans_initiate_register_credit_card'),
 ]
