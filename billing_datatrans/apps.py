884eb7d7a196a32fa3f628d0657780fc59d14f8e
Alberto Pou <bertini36@users.noreply.github.com>
2021-05-11 08:24:01 +0200

    Django 3.2 (#4)
    * Django 3.2 changes
    
    * Update version number

--- billing_datatrans/apps.py
+++ billing_datatrans/apps.py
@@ -5,6 +5,7 @@ from django.apps import AppConfig
 class BillingDatatransConfig(AppConfig):
     name = 'billing_datatrans'
     verbose_name = 'Billing Datatrans'
+    default_auto_field = 'django.db.models.AutoField'
 
     def ready(self):
         # DatatransPSP pulls in datatrans model classes, so we have to wait before we import it.
